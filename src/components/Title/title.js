31a482ab6b7f3fdd3df84054c5e5f0714b39bba1
Sithum Wijesinghe <sithumwijesinghe59@gmail.com>
2024-10-23 20:46:55 +0530

    add cart

---
+++ src/components/Title/title.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function title(title,fontSize,margin) {
+
+  return <h1 style={{fontSize,margin,color:'#616161'}}>{title}</h1>
+}
--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import classes from './CartPage.module.css'
 import { useCart } from '../../hooks/useCart'
+import Title from '../../components/Title/title'
+import { Link } from 'react-router-dom'
 export default function CartPage() {
     const {cart}= useCart();
-  return <div>{cart.items.length}</div>
+  return <>
+  <Title title="cart page" margin='1.5rem 0 0 2.5rem'/>
+
+  {cart && cart.items.length > 0 && 
+  <div className={classes.container}>
+    <ul className={classes.list}>
+        {cart.items.map(item => <li key={item.food.id}>
+            <div>
+                <img onScroll={`/foods/${item.food.imageUrl}`} alt={item.food.name} />
+            </div>
+            <div>
+                <Link to={`/foods/${item.food.id}`}>{item.food.name}</Link>
+            </div>
+        </li>)}
+    </ul>
+  </div>
+  }
+
+  </>
 }
