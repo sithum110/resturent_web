7ee0819bc562d89d460139eac886f46483fa2ca2
Sithum Wijesinghe <sithumwijesinghe59@gmail.com>
2024-10-23 21:28:53 +0530

    add title

--- src/components/Title/title.js
+++ src/components/Title/title.js
@@ -1,6 +1,21 @@
-import React from 'react'
+// import React from 'react'
 
-export default function title(title,fontSize,margin) {
+// export default function title(title,fontSize,margin) {
 
-  return <h1 style={{fontSize,margin,color:'#616161'}}>{title}</h1>
+//   return <h1 style={{fontSize,margin,color:'#616161'}}>{title}</h1>
+// }
+
+
+import React from 'react';
+
+// Title component without passing the entire props object as a child
+function Title(props) {
+  return React.createElement(
+    'h1',
+    { style: { margin: props.margin } }, // Only apply margin style
+    props.title // Pass the title as a valid child (string)
+  );
 }
+
+export default Title;
+
--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,25 +1,78 @@
-import React from 'react'
-import classes from './CartPage.module.css'
-import { useCart } from '../../hooks/useCart'
-import Title from '../../components/Title/title'
-import { Link } from 'react-router-dom'
-export default function CartPage() {
-    const {cart}= useCart();
-  return <>
-  <Title title="cart page" margin='1.5rem 0 0 2.5rem'/>
+// import React from 'react'
+// import classes from './CartPage.module.css'
+// import { useCart } from '../../hooks/useCart'
+// import Title from '../../components/Title/title'
+// import { Link } from 'react-router-dom'
+// export default function CartPage() {
+//     const {cart}= useCart();
+//   return <>
+//   <Title title="cart page" margin='1.5rem 0 0 2.5rem'/>
 
-  {cart && cart.items.length > 0 && 
-  <div className={classes.container}>
-    <ul className={classes.list}>
-        {cart.items.map(item => <li key={item.food.id}>
-            <div>
-                <img onScroll={`/foods/${item.food.imageUrl}`} alt={item.food.name} />
-            </div>
-            <div>
-                <Link to={`/foods/${item.food.id}`}>{item.food.name}</Link>
-            </div>
-            <div>
-                <select value={item.qunatity}>
+//   {cart && cart.items.length > 0 && 
+//   <div className={classes.container}>
+//     <ul className={classes.list}>
+//         {cart.items.map(item => <li key={item.food.id}>
+//             <div>
+//                 <img onScroll={`/foods/${item.food.imageUrl}`} alt={item.food.name} />
+//             </div>
+//             <div>
+//                 <Link to={`/foods/${item.food.id}`}>{item.food.name}</Link>
+//             </div>
+//             <div>
+//                 <select value={item.qunatity}>
+//                     <option>1</option>
+//                     <option>2</option>
+//                     <option>3</option>
+//                     <option>4</option>
+//                     <option>5</option>
+//                     <option>6</option>
+//                     <option>7</option>
+//                     <option>8</option>
+//                     <option>9</option>
+//                     <option>10</option>
+//                 </select>
+//             </div>
+//         </li>)}
+//     </ul>
+//   </div>
+//   }
+
+//   </>
+// }
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Price from '../../components/Price/Price';
+import Title from '../../components/Title/title'
+import { useCart } from '../../hooks/useCart';
+import classes from './CartPage.module.css';
+import NotFound from '../../components/notFound/NotFound';
+export default function CartPage() {
+  const { cart, removeFromCart, changeQuantity } = useCart();
+  return (
+    <>
+      <Title title="Cart Page" margin="1.5rem 0 0 2.5rem" />
+
+      {cart.items.length === 0 ? (
+        <NotFound message="Cart Page Is Empty!" />
+      ) : (
+        <div className={classes.container}>
+          <ul className={classes.list}>
+            {cart.items.map(item => (
+              <li key={item.food.id}>
+                <div>
+                  <img src={`${item.food.imageUrl}`} alt={item.food.name} />
+                </div>
+                <div>
+                  <Link to={`/food/${item.food.id}`}>{item.food.name}</Link>
+                </div>
+
+                <div>
+                  <select
+                    value={item.quantity}
+                    onChange={e => changeQuantity(item, Number(e.target.value))}
+                  >
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -30,12 +83,37 @@ export default function CartPage() {
                     <option>8</option>
                     <option>9</option>
                     <option>10</option>
-                </select>
-            </div>
-        </li>)}
-    </ul>
-  </div>
-  }
+                  </select>
+                </div>
 
-  </>
+                <div>
+                  <Price price={item.price} />
+                </div>
+
+                <div>
+                  <button
+                    className={classes.remove_button}
+                    onClick={() => removeFromCart(item.food.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <div className={classes.checkout}>
+            <div>
+              <div className={classes.foods_count}>{cart.totalCount}</div>
+              <div className={classes.total_price}>
+                <Price price={cart.totalPrice} />
+              </div>
+            </div>
+
+            <Link to="/checkout">Proceed To Checkout</Link>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
