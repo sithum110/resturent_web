66e806706530f44024bf56230e6100a487fe9db0
Sithum Wijesinghe <sithumwijesinghe59@gmail.com>
2024-10-23 20:56:13 +0530

    add count

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -18,6 +18,20 @@ export default function CartPage() {
             <div>
                 <Link to={`/foods/${item.food.id}`}>{item.food.name}</Link>
             </div>
+            <div>
+                <select value={item.qunatity}>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+            </div>
         </li>)}
     </ul>
   </div>
